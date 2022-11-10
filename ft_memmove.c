0d3dc86dcb9980442cc09fd903c4dfad0b76cc7d
1delly <taz12pro@gmail.com>
2022-11-10 16:34:47 +0100

    babe

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,28 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tdelgran <tdelgran@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/10 11:34:45 by tdelgran          #+#    #+#             */
-/*   Updated: 2022/11/10 16:33:11 by tdelgran         ###   ########.fr       */
+/*   Created: 2022/11/10 16:24:22 by tdelgran          #+#    #+#             */
+/*   Updated: 2022/11/10 16:27:16 by tdelgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void    *memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+    size_t i;
 
-	i = 0;
-	if (!dest && !src)
-		return (dest);
-	while (i < n)
-	{
-		((char *)dest)[i] = ((char *)src)[i];
+    i = 0;
+    while (i < n)
+    {
+        ((char *)dest)[i] = ((char *)src)[i];
 		i++;
-	}
-	return (dest);
+    }
 }
