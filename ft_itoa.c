a5a1aef1e529a10d8f68ffab9e8628fbfe0a93c7
1delly <taz12pro@gmail.com>
2022-11-30 11:43:31 +0100

    hehe

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,29 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tdelgran <tdelgran@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/18 16:57:38 by tdelgran          #+#    #+#             */
-/*   Updated: 2022/11/30 10:19:02 by tdelgran         ###   ########.fr       */
+/*   Created: 2022/11/30 10:23:49 by tdelgran          #+#    #+#             */
+/*   Updated: 2022/11/30 10:26:20 by tdelgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+char    *ft_itoa(int n)
 {
-	char	*dst;
-	size_t	i;
-
-	if (!s1)
-		return (0);
-	while (ft_strchr(set, *s1) && *s1)
-		s1++;
-	i = ft_strlen(s1);
-	while (ft_strchr(set, s1[i]) && i)
-		i--;
-	dst = ft_substr((char *)s1, 0, i + 1);
-	return (dst);
+    char    *str;
+    
+    if (!n)
+        return (0);
+    
 }
